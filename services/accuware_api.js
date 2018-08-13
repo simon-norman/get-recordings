ea4b699153255ea609591d90f0ef93eaf65060f2
Simon Norman <01secn@gmail.com>
2018-08-13 17:36:55 +0100

    Module to call accuware api being turned into dumb terminal - just does one get call

--- services/accuware_api.js
+++ services/accuware_api.js
@@ -1,10 +1,44 @@
+import stampit from 'stampit';
 
+module.exports = () => {
+  const accuwareApi = stampit({
+    props: {
+      accuwareBaseUrl: 'https://its.accuware.com/api/v1',
+      baseDeviceLocationsPath: '/sites/siteId/stations/',
+    },
 
-module.exports = (diMockDependency1, diMockDependency2) => {
-  const diMockModule = {
-    diMockDependency1,
-    diMockDependency2,
-  };
-  return diMockModule;
+    init({
+      axios,
+      siteId,
+      intervalPeriodInSeconds,
+      includeLocations = 'yes',
+      devicesToInclude = 'all',
+      areas = 'yes',
+    }) {
+      this.axios = axios.create({
+        baseURL: this.accuwareBaseUrl,
+        responseType: 'json',
+      });
+      this.locationsCallParams.setFinalDeviceLocationsPath(siteId);
+      this.locationsCallParams.lrrt = intervalPeriodInSeconds;
+      this.locationsCallParams.loc = includeLocations;
+      this.locationsCallParams.type = devicesToInclude;
+      this.locationsCallParams.areas = areas;
+    },
+
+    methods: {
+      registerForDeviceLocations() {
+        this.axios.get(
+          `${this.finalDeviceLocationsPath}`,
+          this.locationsCallParams,
+        );
+      },
+
+      setFinalDeviceLocationsPath(siteId) {
+        this.finalDeviceLocationsPath = this.baseDeviceLocationsPath.replace('siteId', siteId);
+      },
+    },
+  });
+  return accuwareApi;
 };
 
