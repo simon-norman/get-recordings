5ef41bbe4a630033682e22a4fa92200c24062894
Simon Norman <01secn@gmail.com>
2018-08-20 18:00:22 +0100

    Started parser to convert accuware data

--- app.js
+++ app.js
@@ -1,12 +1,6 @@
 
 const { wireUpApp } = require('./dependency_injection/app_wiring');
-const express = require('express');
 
 wireUpApp();
 
-const app = express();
-const port = 500;
 
-app.listen(port);
-
-console.log(`Track api listening on port ${port}`);
---
+++ services/accuware_recordings_parser_test.js
@@ -0,0 +1,16 @@
+
+
+const { expect } = require('chai');
+const mongoose = require('mongoose');
+const { getConfigForEnvironment } = require('../config/config.js');
+const Recording = require('./recording.js');
+
+describe('accuware_recordings_parser_test', () => {
+
+  it('should convert accuware recordings into standard recording format', async function () {
+  });
+
+  it('should validate that the accuware recordings have all required data', async function () {
+  });
+});
+
