85c749051713d725ae911cd88eefabc8128de592
Simon Norman <01secn@gmail.com>
2018-08-24 18:58:54 +0100

    Sandbox updated

--- sandbox.js
+++ sandbox.js
@@ -12,3 +12,27 @@
   .catch((error) => {
 
   });
+
+  this.setFinalDeviceLocationsPath(siteId);
+  /* this.locationsCallParams.params = {
+    lrrt: intervalPeriodInSeconds,
+    loc: includeLocations
+  } */
+  this.locationsCallParams.params.lrrt = intervalPeriodInSeconds;
+  this.locationsCallParams.params.loc = includeLocations;
+  this.locationsCallParams.params.type = devicesToInclude;
+  this.locationsCallParams.params.areas = areas;
+},
+
+const privateMethod = Symbol('privateMethod');
+
+methods: {
+  [privateMethod]() {
+    // test private method
+  },
+  getDeviceLocations() {
+    const timestampCallMade = Date.now();
+
+    const response = this.get(
+      this.finalDeviceLocationsPath,
+      this.locationsCallParams,
